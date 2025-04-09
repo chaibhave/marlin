fa311fbddee883b1c5b40e91b06ebd84ff012171
ChaitanyaBhave-e <chaitanya.bhave@inl.gov>
2025-04-09 14:58:25 -0600

    Memory efficient RK4 implementation, removes extra stored variables

--- src/tensor_solver/ExplicitRungeKuttaSolver.C
+++ src/tensor_solver/ExplicitRungeKuttaSolver.C
@@ -18,7 +18,7 @@ ExplicitRungeKuttaSolver::validParams()
   InputParameters params = ExplicitSolverBase::validParams();
   params.addClassDescription("Semi-implicit time integration solver.");
   params.addParam<unsigned int>("substeps", 1, "Explicit substeps per time step.");
-  params.addParam<unsigned int>("order",4,"number of stages in Runge-Kutta time integration.")
+  params.addParam<unsigned int>("order",4,"number of stages in Runge-Kutta time integration.");
   return params;
 }
 
@@ -31,111 +31,60 @@ ExplicitRungeKuttaSolver::ExplicitRungeKuttaSolver(const InputParameters & param
 {
 }
 
-// void
-// ExplicitRungeKuttaSolver::computeBuffer()
-// {
-//   torch::Tensor ubar;
-//   _sub_dt = _dt / _substeps;
-
-//   // subcycles
-//   for (const auto substep : make_range(_substeps))
-//   {
-//     // re-evaluate the solve compute
-//     _compute->computeBuffer();
-//     // integrate all variables
-//     for (auto & [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
-//     {
-//       ubar = reciprocal_buffer + _sub_dt * time_derivative_reciprocal;
-//       u = _domain.ifft(ubar);
-//     }
-
-//     if (substep < _substeps - 1)
-//       _tensor_problem.advanceState();
-
-//     // increment substep time
-//     _sub_time += _sub_dt;
-//   }
-// }
-
 void
 ExplicitRungeKuttaSolver::computeBuffer()
 {
-  torch::Tensor k1, k2, k3, k4;
-  std::vector<torch::Tensor> u0, u_temp;
+  std::vector<torch::Tensor> y_current_hat(_variables.size()), y_next_hat(_variables.size());
   _sub_dt = _dt / _substeps;
 
   // subcycles
   for (const auto substep : make_range(_substeps))
+{
+  _compute->computeBuffer();
+  // Stage 1
+  std::size_t i = 0;
+  for (auto& [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
   {
-    // Save the initial state for all variables
-    u0.clear();
-    for (auto & [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
-    {
-      u0.push_back(u.clone());
-    }
-
-    // Stage 1
-    _compute->computeBuffer();
-    std::vector<torch::Tensor> k1_list;
-    for (auto & [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
-    {
-      k1_list.push_back(_sub_dt * time_derivative_reciprocal);
-    }
-
-    // Stage 2
-    for (size_t i = 0; i < _variables.size(); ++i)
-    {
-      auto & [u, reciprocal_buffer, time_derivative_reciprocal] = _variables[i];
-      u_temp.push_back(_domain.ifft(reciprocal_buffer + 0.5 * k1_list[i]));
-      u = u_temp.back();
-    }
-    _compute->computeBuffer();
-    std::vector<torch::Tensor> k2_list;
-    for (auto & [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
-    {
-      k2_list.push_back(_sub_dt * time_derivative_reciprocal);
-    }
-
-    // Stage 3
-    for (size_t i = 0; i < _variables.size(); ++i)
-    {
-      auto & [u, reciprocal_buffer, time_derivative_reciprocal] = _variables[i];
-      u_temp[i] = _domain.ifft(reciprocal_buffer + 0.5 * k2_list[i]);
-      u = u_temp[i];
-    }
-    _compute->computeBuffer();
-    std::vector<torch::Tensor> k3_list;
-    for (auto & [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
-    {
-      k3_list.push_back(_sub_dt * time_derivative_reciprocal);
-    }
-
-    // Stage 4
-    for (size_t i = 0; i < _variables.size(); ++i)
-    {
-      auto & [u, reciprocal_buffer, time_derivative_reciprocal] = _variables[i];
-      u_temp[i] = _domain.ifft(reciprocal_buffer + k3_list[i]);
-      u = u_temp[i];
-    }
-    _compute->computeBuffer();
-    std::vector<torch::Tensor> k4_list;
-    for (auto & [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
-    {
-      k4_list.push_back(_sub_dt * time_derivative_reciprocal);
-    }
-
-    // Combine stages
-    for (size_t i = 0; i < _variables.size(); ++i)
-    {
-      auto & [u, reciprocal_buffer, time_derivative_reciprocal] = _variables[i];
-      ubar = reciprocal_buffer + (k1_list[i] + 2 * k2_list[i] + 2 * k3_list[i] + k4_list[i]) / 6.0;
-      u = _domain.ifft(ubar);
-    }
-
-    if (substep < _substeps - 1)
-      _tensor_problem.advanceState();
-
-    // increment substep time
-    _sub_time += _sub_dt;
+      y_current_hat[i] = reciprocal_buffer;
+      y_next_hat[i] = reciprocal_buffer +  time_derivative_reciprocal * _sub_dt / 6;
+      u = _domain.ifft(reciprocal_buffer + time_derivative_reciprocal * _sub_dt / 2);
+      ++i;
   }
+  _compute->computeBuffer();
+
+  // Stage 2
+  i = 0;
+  for (auto& [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
+  {
+      y_next_hat[i] += time_derivative_reciprocal * _sub_dt / 3;
+      u = _domain.ifft(y_current_hat[i] + time_derivative_reciprocal * _sub_dt / 2);
+      ++i;
+  }
+  _compute->computeBuffer();
+
+  // Stage 3
+  i = 0;
+  for (auto& [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
+  {
+      y_next_hat[i] += time_derivative_reciprocal * _sub_dt / 3;
+      u = _domain.ifft(y_current_hat[i] + time_derivative_reciprocal * _sub_dt);
+      ++i;
+  }
+  _compute->computeBuffer();
+
+  // Stage 4
+  i = 0;
+  for (auto& [u, reciprocal_buffer, time_derivative_reciprocal] : _variables)
+  {
+      u = _domain.ifft(y_next_hat[i] + time_derivative_reciprocal * _sub_dt / 6);
+      ++i;
+  }
+  _compute->computeBuffer();
+
+  if (substep < _substeps - 1)
+    _tensor_problem.advanceState();
+
+  // increment substep time
+  _sub_time += _sub_dt;
+}
 }
